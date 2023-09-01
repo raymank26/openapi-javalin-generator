d119628241f477c2d7dbde276955d32b36da34dd
Anton Ermak <Rayman.k26@gmail.com>
2023-09-02 02:41:17 +0700

    Init commit.

---
+++ src/functionalTest/kotlin/com/github/raymank26/functional/FunctionalTest.kt
@@ -0,0 +1,32 @@
+package com.github.raymank26.functional
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+class FunctionalTest {
+
+    @TempDir
+    lateinit var projectDir: File
+
+    @Test
+    fun test() {
+        File(projectDir, "build.gradle").writer().use { writer ->
+            writer.write(
+                """
+                plugins {
+                    id 'com.github.raymank26.javalin-swagger'
+                }
+            """.trimIndent()
+            )
+
+        }
+        GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withArguments("generateSwaggerClient")
+            .forwardOutput()
+            .withPluginClasspath()
+            .build()
+    }
+}
---
+++ src/main/kotlin/com/github/raymank26/JavalinSwaggerPlugin.kt
@@ -0,0 +1,12 @@
+package com.github.raymank26
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class JavalinSwaggerPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.tasks.create("generateSwaggerClient") {
+        }
+    }
+}
