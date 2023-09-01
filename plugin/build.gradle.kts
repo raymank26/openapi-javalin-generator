980b1622d1d68ba7e998b5f8478f8fba6ed6becd
Anton Ermak <Rayman.k26@gmail.com>
2023-09-02 02:45:32 +0700

    build.gradle.kts added.

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    `java-gradle-plugin`
+}
+
+group = "com.github.raymank26"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val functionalTest: SourceSet = sourceSets.create("functionalTest")
+
+val functionalTestTask = tasks.register<Test>("functionalTest") {
+    group = "verification"
+    testClassesDirs = functionalTest.output.classesDirs
+    classpath = functionalTest.runtimeClasspath
+    useJUnitPlatform()
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.check {
+    dependsOn(functionalTestTask)
+}
+
+gradlePlugin {
+    // Define the plugin
+    val swaggerGenerator by plugins.creating {
+        id = "com.github.raymank26.javalin-swagger"
+        implementationClass = "com.github.raymank26.JavalinSwaggerPlugin"
+    }
+    testSourceSets(functionalTest)
+
+}
+
+dependencies {
+    "functionalTestImplementation"("org.junit.jupiter:junit-jupiter:5.10.0")
+    "functionalTestImplementation"("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    "functionalTestImplementation"("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    "functionalTestImplementation"(project)
+    "functionalTestImplementation"(gradleTestKit())
+    "functionalTestRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(8)
+}
