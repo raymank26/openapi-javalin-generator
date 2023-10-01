b7e402805fececb3f207f17c416c8a0c8433ce7f
Anton Ermak <Rayman.k26@gmail.com>
2023-10-01 14:41:23 +0700

    Method extracted.

--- plugin/src/main/kotlin/com/github/raymank26/JavalinControllerGenerator.kt
+++ plugin/src/main/kotlin/com/github/raymank26/JavalinControllerGenerator.kt
@@ -70,98 +70,7 @@ class JavalinControllerGenerator(
                             operationDescriptor.path
                         )
                             .withIndent {
-                                val requestBodyBlock = if (requestBody != null) {
-                                    buildCodeBlock {
-                                        addStatement("val body = when (ctx.contentType()?.split(\";\")?.first()) {")
-                                        withIndent {
-                                            requestBody.contentTypeToType.forEach { (key, value): Map.Entry<RequestBodyMediaType, TypeDescriptor> ->
-                                                val parser = when (key) {
-                                                    RequestBodyMediaType.FormData -> buildCodeBlock {
-                                                        val targetObj = (value as TypeDescriptor.Object)
-                                                        add(
-                                                            "%T(%T(",
-                                                            ClassName(
-                                                                basePackageName,
-                                                                requestBody.clsName,
-                                                                key.clsName
-                                                            ),
-                                                            ClassName(basePackageName, targetObj.clsName)
-                                                        )
-                                                        withIndent(3) {
-                                                            targetObj.properties.forEach { property ->
-                                                                add("ctx.formParam(%S)", property.name)
-                                                                add(if (property.required) "!!" else "?")
-                                                                when (property.type) {
-                                                                    TypeDescriptor.Int64Type -> addStatement(".toLong(),")
-                                                                    TypeDescriptor.IntType -> addStatement(".toInt(),")
-                                                                    TypeDescriptor.StringType -> addStatement(".toString(),")
-                                                                    else -> error("Not supported type = ${property.type}")
-                                                                }
-                                                            }
-                                                        }
-                                                        addStatement("))")
-                                                    }
-
-                                                    RequestBodyMediaType.Json -> buildCodeBlock {
-                                                        addStatement(
-                                                            "%T(ctx.bodyAsClass(%L::class.java))",
-                                                            ClassName(
-                                                                basePackageName,
-                                                                requestBody.clsName,
-                                                                key.clsName
-                                                            ),
-                                                            ClassName(
-                                                                basePackageName,
-                                                                (value as TypeDescriptor.Object).clsName
-                                                            )
-                                                        )
-                                                    }
-
-                                                    RequestBodyMediaType.Xml -> buildCodeBlock {
-                                                        addStatement("TODO(\"Not implemented\")")
-                                                    }
-                                                }
-                                                add("%S -> ", key.mediaType)
-                                                add(parser)
-                                            }
-                                            addStatement("else -> error(\"No request found\")")
-                                        }
-                                        addStatement("}")
-                                    }
-                                } else {
-                                    CodeBlock.builder().build()
-                                }
-                                add(requestBodyBlock)
-                                add("val response = server.%L", operationDescriptor.operationId)
-                                add(parameters)
-
-                                add(buildCodeBlock {
-                                    addStatement("when (response) {")
-                                        .withIndent {
-                                            operationDescriptor.responseBody.statusCodeToClsName.forEach { (code, option) ->
-                                                val sealedClsName = ClassName(
-                                                    basePackageName,
-                                                    operationDescriptor.responseBody.clsName
-                                                )
-                                                addStatement("is %T.%L -> {", sealedClsName, option.clsName)
-                                                    .withIndent {
-                                                        val statusCode = if (code == "default")
-                                                            "response.${option.clsName.decapitalized()}.code"
-                                                        else code
-                                                        addHeaders(option)
-                                                        addStatement("ctx.status(%L)", statusCode)
-                                                        if (option is ResponseBodySealedOption.Parametrized) {
-                                                            addStatement(
-                                                                "ctx.json(response.%L)",
-                                                                option.clsName.decapitalized()
-                                                            )
-                                                        }
-                                                    }
-                                                    .addStatement("}")
-                                            }
-                                        }
-                                        .addStatement("}")
-                                })
+                                addResponseProcessingCode(requestBody, operationDescriptor, parameters)
                             }
                             .addStatement("}")
                     }
@@ -173,6 +82,105 @@ class JavalinControllerGenerator(
             .addType(typeBuilder.build())
             .build()
             .writeTo(baseGenerationPath)
+    }
+
+    private fun CodeBlock.Builder.addResponseProcessingCode(
+        requestBody: RequestBody?,
+        operationDescriptor: OperationDescriptor,
+        parameters: CodeBlock
+    ) {
+        val requestBodyBlock = if (requestBody != null) {
+            buildCodeBlock {
+                addStatement("val body = when (ctx.contentType()?.split(\";\")?.first()) {")
+                withIndent {
+                    requestBody.contentTypeToType.forEach { (key, value): Map.Entry<RequestBodyMediaType, TypeDescriptor> ->
+                        val parser = when (key) {
+                            RequestBodyMediaType.FormData -> buildCodeBlock {
+                                val targetObj = (value as TypeDescriptor.Object)
+                                add(
+                                    "%T(%T(",
+                                    ClassName(
+                                        basePackageName,
+                                        requestBody.clsName,
+                                        key.clsName
+                                    ),
+                                    ClassName(basePackageName, targetObj.clsName)
+                                )
+                                withIndent(3) {
+                                    targetObj.properties.forEach { property ->
+                                        add("ctx.formParam(%S)", property.name)
+                                        add(if (property.required) "!!" else "?")
+                                        when (property.type) {
+                                            TypeDescriptor.Int64Type -> addStatement(".toLong(),")
+                                            TypeDescriptor.IntType -> addStatement(".toInt(),")
+                                            TypeDescriptor.StringType -> addStatement(".toString(),")
+                                            else -> error("Not supported type = ${property.type}")
+                                        }
+                                    }
+                                }
+                                addStatement("))")
+                            }
+
+                            RequestBodyMediaType.Json -> buildCodeBlock {
+                                addStatement(
+                                    "%T(ctx.bodyAsClass(%L::class.java))",
+                                    ClassName(
+                                        basePackageName,
+                                        requestBody.clsName,
+                                        key.clsName
+                                    ),
+                                    ClassName(
+                                        basePackageName,
+                                        (value as TypeDescriptor.Object).clsName
+                                    )
+                                )
+                            }
+
+                            RequestBodyMediaType.Xml -> buildCodeBlock {
+                                addStatement("TODO(\"Not implemented\")")
+                            }
+                        }
+                        add("%S -> ", key.mediaType)
+                        add(parser)
+                    }
+                    addStatement("else -> error(\"No request found\")")
+                }
+                addStatement("}")
+            }
+        } else {
+            CodeBlock.builder().build()
+        }
+        add(requestBodyBlock)
+        add("val response = server.%L", operationDescriptor.operationId)
+        add(parameters)
+
+        add(buildCodeBlock {
+            addStatement("when (response) {")
+                .withIndent {
+                    operationDescriptor.responseBody.statusCodeToClsName.forEach { (code, option) ->
+                        val sealedClsName = ClassName(
+                            basePackageName,
+                            operationDescriptor.responseBody.clsName
+                        )
+                        addStatement("is %T.%L -> {", sealedClsName, option.clsName)
+                            .withIndent {
+                                val statusCode = if (code == "default")
+                                    "response.${option.clsName.decapitalized()}.code"
+                                else code
+                                addHeaders(option)
+                                addStatement("ctx.status(%L)", statusCode)
+                                if (option is ResponseBodySealedOption.Parametrized) {
+                                    addStatement(
+                                        "ctx.json(response.%L)",
+                                        option.clsName.decapitalized()
+                                    )
+                                }
+                            }
+                            .addStatement("}")
+                    }
+                }
+                .addStatement("}")
+        })
     }
 }
 
@@ -199,7 +207,7 @@ private fun CodeBlock.Builder.addHeaders(option: ResponseBodySealedOption) {
     }
 }
 
-public inline fun CodeBlock.Builder.withIndent(
+inline fun CodeBlock.Builder.withIndent(
     steps: Int = 1,
     builderAction: CodeBlock.Builder.() -> Unit
 ): CodeBlock.Builder {
